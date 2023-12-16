cbdfe5c8caa81f38375cbb71776f33dcade72718
callofnoob304 <callofnoob712@gmail.com>
2023-12-15 22:09:25 -0300

    desenvolvendo a class pedido

---
+++ src/Pedidos.java
@@ -0,0 +1,15 @@
+import lanche from './Lanche.java';
+
+public class Pedidos {
+  var pedido = new pedido();
+
+  pedido.codigo = 123;
+  pedido.comida = lanche.nome;
+  pedido.preco = lanche.valor;
+}
+
+class pedido {
+  int codigo;
+  String comida;
+  Double preco;
+}
